76294d3046b7655f2dd253060812e5050563e2ff
JuanMiguelSantamaríaMúnera <jumsanta@nequi.com>
2026-07-28 17:18:27 -0500

    refactor: salida descriptiva y finalizacion de error en business

--- src/business/business.js
+++ src/business/business.js
@@ -1,9 +1,3 @@
-const nequiUtils = require('@nequi/nequi-utils')
-const nequiApiUtils = require('@nequi/nequi-api-utils')
-const responseUtils = nequiApiUtils.ResponseAPIUtils
-const RESPONSE_MESSAGES = nequiApiUtils.RESPONSE_MESSAGES
-const env = nequiUtils.Environment
-const lambdaUtils = nequiUtils.Lambda8
 
 const {
   service
@@ -14,21 +8,8 @@ module.exports = async function processBusiness (event) {
 }
 
 const callService = async (event) => {
-  try {
-    // Implementar Logica de negocio
-    return await service(event)
-  } catch (error) {
-    if (!!error && !!error.output) {
-      throw error
-    } else {
-      throw lambdaUtils.buildOutput(true, true,
-        getOutput(event, RESPONSE_MESSAGES.TECHNICAL_ERROR.CODE,
-          RESPONSE_MESSAGES.TECHNICAL_ERROR.DESCRIPTION),
-        'Sistema que fallo', 'proceso o función', error)
-    }
-  }
+ 
+  return await service(event)
+  
 }
 
-const getOutput = (event, code, description, body) => {
-  return responseUtils.buildResponseFromRequest(event, code, description, body)
-}
