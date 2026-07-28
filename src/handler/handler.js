0ebaa56d551340da7834f1324492ed2c5475fbb3
JuanMiguelSantamaríaMúnera <jumsanta@nequi.com>
2026-07-28 17:18:23 -0500

    refactor: trazabilidad por MessageID y logging por variable de entorno

--- src/handler/handler.js
+++ src/handler/handler.js
@@ -1,4 +1,3 @@
-const { randomUUID } = require('crypto')
 const lambdaUtils = require('@nequi/nequi-utils').Lambda8
 const nequiApiUtils = require('@nequi/nequi-api-utils')
 const responseUtils = nequiApiUtils.ResponseAPIUtils
@@ -7,19 +6,22 @@ const RESPONSE_MESSAGES = nequiApiUtils.RESPONSE_MESSAGES
 const validateSchema = require('../config/schema/validateSchema')
 const processBusiness = require('../business/business')
 
-module.exports = async function defaultHandler (event, context) {
-  const traceID = randomUUID()
-  event.traceID = traceID
-  lambdaUtils.log(`reto_serverless entrada [trace:${traceID}]`, event, true)
+module.exports = async function defaultHandler(event, context) {
   try {
+    let traceID = event.RequestMessage && event.RequestMessage.RequestHeader
+      ? event.RequestMessage.RequestHeader.MessageID
+      : undefined
+    event.traceID = traceID
+    lambdaUtils.log(`reto_serverless entrada [trace:${traceID}]`, event)
+
+    
     await validateSchema(event)
     const response = await processBusiness(event)
     return await lambdaUtils.finish(lambdaUtils.buildOutput(true, false,
       getOutput(event, RESPONSE_MESSAGES.SUCCESS.CODE,
         RESPONSE_MESSAGES.SUCCESS.DESCRIPTION, { parametersRS: response })
-      ))
+    ))
   } catch (error) {
-    lambdaUtils.log(`reto_serverless error [trace:${traceID}]`, error, true)
     return await lambdaUtils.finish(error)
   }
 }
